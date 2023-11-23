484240bf06537d0520dcf7064fd03741b7be42b9
bduhamel <benjamin.duhamel@ens-lyon.fr>
2023-11-23 12:05:30 +0100

    recipe example

--- gameClass.py
+++ gameClass.py
@@ -1,4 +1,6 @@
 import random
+
+import console
 from config import default_config
 import place
 import item
@@ -26,7 +28,24 @@ class Game:
         assert 0 <= probability <= 1
         return rd.random() < probability
 
-    def item_creation(self):
+    def check_ingredients(self, recipe, nb_prod):
+        for (item, i) in recipe.ingredients:
+            if item not in self.inventory or self.inventory[item] < i * nb_prod:
+                return False
+        return True
+
+    def item_creation(self, nb_prod=1):
         for recipe in self.known_recipe:
-            if self.check_ingredients(recipe, self.inventory):
-                print(f"Create {recipe.product}")
+            if self.check_ingredients(recipe, nb_prod):
+                if self.io_mode == "console":
+                    ans = console.answer_yn(f"Create {nb_prod} {recipe.product}? :")
+                else:
+                    raise NotImplementedError
+                if ans:
+                    for (item, i) in recipe.ingredients:
+                        self.inventory[item] -= i * nb_prod
+                        if self.inventory[item] == 0:
+                            del self.inventory[item]
+                    for (item, i) in recipe.products:
+                        self.inventory[item] += i
+                print(f"{recipe.product} have been created!")
